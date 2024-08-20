dd19706b311df9428a97182566c0b11bae9898fe
Criativaria <mariaritaboareto@gmail.com>
2024-08-20 00:06:16 -0300

    fix: overwrite bugs in channels storage

--- src/utils/storage/channels.ts
+++ src/utils/storage/channels.ts
@@ -5,33 +5,26 @@ const FAVORITE_CHANNELS_KEY = "FAVORITE_CHANNELS";
 export async function addFavoriteChannel(channelId: string) {
     try {
         const favoritesChannels: string[] = await getFavoritesChannels();
-        if (favoritesChannels) {
-            if (favoritesChannels.includes(channelId)) {
-                return;
-            }
+
+        if (!favoritesChannels.includes(channelId)) {
             favoritesChannels.push(channelId)
             await AsyncStorage.setItem(FAVORITE_CHANNELS_KEY, JSON.stringify(favoritesChannels))
         }
-
-        await AsyncStorage.setItem(FAVORITE_CHANNELS_KEY, JSON.stringify([channelId]));
-
     } catch (error) {
         console.error("deu erro no addFavoritesChannels: " + error)
     }
 
 }
-
 export async function getFavoritesChannels(): Promise<string[]> {
     try {
         const favoritesChannels = await AsyncStorage.getItem(FAVORITE_CHANNELS_KEY)
-        return JSON.parse(favoritesChannels!);
+        return JSON.parse(favoritesChannels!) || []
     } catch (error) {
         console.error("deu erro no getFavoritesChannels: " + error)
         return [];
     }
 
 }
-
 export async function removeFavoriteChannel(channelId: string) {
     try {
         const favoritesChannels: string[] = await getFavoritesChannels();
