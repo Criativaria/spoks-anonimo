37860fba21960aabb69df554a83e12bff5d33089
gustmnds <37991797+gustmnds@users.noreply.github.com>
2024-08-24 18:36:57 -0300

    feat: create programs api mocks

---
+++ src/api/programs/types.ts
@@ -0,0 +1,12 @@
+export type Program = {
+    id: number;
+    code: string;
+    name: string;
+    category: string;
+    channel_id: string;
+    start_at: string;
+    end_at: string;
+    created_at: string;
+};
+
+export type GetPrograms = (channelId: string) => Promise<Program[]>;
