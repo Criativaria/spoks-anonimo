9addf639e131cd7c5f2248c1355e40eb11370996
Criativaria <mariaritaboareto@gmail.com>
2024-09-07 18:53:02 -0300

    fix: name of conection with the database in the .env

--- src/api/programs/index.ts
+++ src/api/programs/index.ts
@@ -2,6 +2,6 @@ import * as integration from "./integration"
 import * as mock from "./mock"
 import { GetPrograms } from "./types";
 
-const selected = process.env.NODE_ENV === "production" ? integration : mock;
+const selected = process.env.EXPO_PUBLIC_ENV === "production" ? integration : mock;
 
 export const getPrograms: GetPrograms = selected.getPrograms;
--- src/api/programs/integration.ts
+++ src/api/programs/integration.ts
@@ -2,15 +2,15 @@ import axios, { AxiosError } from "axios";
 import { Program, GetPrograms } from "./types";
 
 export const getPrograms: GetPrograms = async (channelCode: string) => {
-    const API_URL: string = process.env.API_URL!;
-    
+    const API_URL: string = process.env.EXPO_PUBLIC_API_URL!;
+
     try {
         const axiosResult = await axios.get(`${API_URL}/program/${channelCode}`);
         const programsResult: Program[] = axiosResult.data || [];
 
         return programsResult;
     } catch (error) {
-        if(error instanceof AxiosError) console.error({code: error.code, message: error.message});
+        if (error instanceof AxiosError) console.error({ code: error.code, message: error.message });
         else console.error(error);
 
         return [];
